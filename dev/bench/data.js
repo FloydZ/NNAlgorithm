a8bcdaaa9a32be371b33cd6405809664681b96f9
github-action-benchmark <github@users.noreply.github.com>
2022-11-19 20:55:42 +0000

    add gBenchmark (googlecpp) benchmark result for 04cbe11419f5b279d96779848a82c01e192e8b13

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1668891342231,
+  "repoUrl": "https://github.com/FloydZ/NNAlgorithm",
+  "entries": {
+    "gBenchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "committer": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "distinct": true,
+          "id": "04cbe11419f5b279d96779848a82c01e192e8b13",
+          "message": "kek",
+          "timestamp": "2022-11-19T21:32:37+01:00",
+          "tree_id": "aa58bacdc9f85579de96fd9a5e01e50dbdbaf8d9",
+          "url": "https://github.com/FloydZ/NNAlgorithm/commit/04cbe11419f5b279d96779848a82c01e192e8b13"
+        },
+        "date": 1668891341822,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_quad",
+            "value": 152941.1908366534,
+            "unit": "ns/iter",
+            "extra": "iterations: 10040\ncpu: 152924.43227091632 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
