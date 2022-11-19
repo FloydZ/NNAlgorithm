5ea2053d8d89c5ca2a2d98b86b73366a9970353c
github-action-benchmark <github@users.noreply.github.com>
2022-11-19 21:05:26 +0000

    add gBenchmark (googlecpp) benchmark result for bb7ffeb3c11b27b14a8542b292ba717ebb2a02de

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668891342231,
+  "lastUpdate": 1668891926813,
   "repoUrl": "https://github.com/FloydZ/NNAlgorithm",
   "entries": {
     "gBenchmark": [
@@ -28,6 +28,34 @@ window.BENCHMARK_DATA = {
             "value": 152941.1908366534,
             "unit": "ns/iter",
             "extra": "iterations: 10040\ncpu: 152924.43227091632 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "committer": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "distinct": true,
+          "id": "bb7ffeb3c11b27b14a8542b292ba717ebb2a02de",
+          "message": "updated readmde",
+          "timestamp": "2022-11-19T22:04:11+01:00",
+          "tree_id": "c0b13e8706a7b2350bca3a922ef7f193e0d798fa",
+          "url": "https://github.com/FloydZ/NNAlgorithm/commit/bb7ffeb3c11b27b14a8542b292ba717ebb2a02de"
+        },
+        "date": 1668891926376,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_quad",
+            "value": 132423.4083942444,
+            "unit": "ns/iter",
+            "extra": "iterations: 4241\ncpu: 132400.8488564018 ns\nthreads: 1"
           }
         ]
       }
